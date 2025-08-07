a8d7f83128c955bf42a0729c5479d363f34c0f3b
Sunil Dhankhar <sunilkumardhankhar@gmail.com>
2025-08-07 16:21:31 +0530

    new project documentation on NextJs

---
+++ Next.js
@@ -0,0 +1,20 @@
+/*          Self Documentation 
+
+    // Next js is a React Freamwork for full stack web Application that is used to build Interactive, Dynamic and Fast React Application.
+    // You use React Components to build user Interface (design) and Next.js for Additional feature and optimization.
+    // you can intead focus on building your product and Shipping Quickly.
+
+    // Next.js have two type of Routers
+    
+    // 1.) App router (New):=>  * Support Server Cmponents
+    //                          * It Uses New Experimental and latest Stable Features.
+    //                          * Keep Seprate  to Server and client  Components for Better Handling.
+    //                          * Better for New Projects
+
+    // 2.) Pages Router(Old):=> * its a Original Routing System.
+    //                          * In this Router Every page in a folder is a file  like /pages => pages/about.us => 
+    //                          * Here is Version Dependency because it use the React version that is Installed in your Project if you have Installed
+                                  17th Version of React then you cant use the latest feature of 18th Version of React untill you have not  Update it Manually with Latest Version of React.
+    //                          * 
+
+*/
